1999a644d2f51b7f640aa082cc7732cc89f5d7ac
LeTanThanh <tanthanhcr95@gmail.com>
2019-03-29 18:51:46 +0700

    add_users#destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
-  before_action :load_user, only: %i|show|
+  before_action :load_user, only: %i|show destroy|
 
   def index
     @users = User.all
   end
 
   def show
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = "User is deleted"
+    else
+      flash[:danger] = "User isn't deleted"
+    end
+
+    redirect_to root_url
   end
 
   private
