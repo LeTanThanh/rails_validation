2303d6f7c15f3eb9f7b7520a55b2449aaf220e37
LeTanThanh <tanthanhcr95@gmail.com>
2019-03-29 18:45:04 +0700

    add_users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: %i|show|
+
   def index
     @users = User.all
+  end
+
+  def show
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:warning] = "User not found"
+    redirect_to root_url
   end
 end
