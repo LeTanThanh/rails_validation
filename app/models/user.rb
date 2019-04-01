29cb3e2405b97f7a2c79ab22e2e41bad59d7b4fe
LeTanThanh <tanthanhcr95@gmail.com>
2019-04-01 20:59:09 +0700

    add_validates_associated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :books, dependent: :destroy
+
   validates :name, presence: true
   validates :terms_of_service, acceptance: true
+  validates_associated :books
 end
